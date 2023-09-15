cd552446236c34b10c3022c8070565a7ce26e687
alaa shaher <alaashaher12@gmail.com>
2023-09-15 21:08:10 +0300

    guard route

---
+++ src/views/LoginView.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { createErrorHandler, createSubmitHandler, useForm } from 'vue-use-form'
+import * as yup from 'yup'
+// import { useYupResolver } from '@vue-use-form/yup'
+
+// const schema = yup.object().shape({
+//     username: yup.string().required(),
+//     age: yup.number().required().positive().integer(),
+//     email: yup.string().email(),
+// })
+
+const {
+    register,
+    formState: { errors },
+    handleSubmit,
+} = useForm({
+    // resolver: useYupResolver(schema),
+    mode: 'all',
+    defaultValues: {
+        name: '',
+        password: '',
+        // remember: true
+    }
+})
+const onSubmit = createSubmitHandler((data) => {
+    console.log(data)
+})
+
+const onError = createErrorHandler((errors) => {
+    console.log(errors)
+})
+</script>
+
+<template>
+    <div class="login-page">
+        <div class="custom-container">
+            <div class="page-wrapper">
+                <div class="img-wrap">
+                    <img src="../assets/images/signin.svg" alt="signin" />
+                </div>
+                <div class="form-top-level-wrapper">
+                    <form class="login-form" @submit.prevent="handleSubmit(onSubmit, onError)()">
+                        <div class="form-header">
+                            <p>Log in</p>
+                        </div>
+
+                        <div class="form-body">
+                            <input :="register('name', { required: true })" type="text">
+                            <input :="register('password', { required: true })" type="password">
+                            <button type="submit" class="submit-btn">
+                                log in
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
