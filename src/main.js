e60d6f152f9f8ef4d7651786851ffc64bbb0a284
alaa shaher <alaashaher12@gmail.com>
2023-09-17 15:22:23 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+import 'ant-design-vue/dist/reset.css'
 import './assets/main.scss'
+import Antd from 'ant-design-vue'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -6,6 +8,5 @@ import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app.use(Antd).use(router).mount('#app')
+// app.mount('#app')
